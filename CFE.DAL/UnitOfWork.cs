affac154ff5e25bcbb809e93a374f22868be8ac7
Kyrylo Voronkov <kavoronkov@gmail.com>
2020-03-14 18:43:47 +0200

    14032020_repository

---
+++ CFE.DAL/UnitOfWork.cs
@@ -0,0 +1,128 @@
+﻿using CFE.DAL.Context;
+using CFE.DAL.Repositories;
+using CFE.Entities.Models;
+using CFE.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFE.DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationContext applicationContext;
+        private UserRepository userRepository;
+        private FormRepository formRepository;
+        private QuestionRepository questionRepository;
+        private AnswerRepository answerRepository;
+        private ElementRepository elementRepository;
+        private AttributeRepository attributeRepository;
+        private FormResultRepository formResultRepository;
+        private QuestionResultRepository questionResultRepository;
+        private AnswerResultRepository answerResultRepository;
+        private AttributeResultRepository attributeResultRepository;
+        public UnitOfWork()
+        {
+            applicationContext = new ApplicationContext();
+        }
+
+        public IRepository<User> Users
+        {
+            get
+            {
+                if (userRepository == null)
+                    userRepository = new UserRepository(applicationContext);
+                return userRepository;
+            }
+        }
+        public IRepository<Form> Forms
+        {
+            get
+            {
+                if (formRepository == null)
+                    formRepository = new FormRepository(applicationContext);
+                return formRepository;
+            }
+        }
+        public IRepository<Question> Questions
+        {
+            get
+            {
+                if (questionRepository == null)
+                    questionRepository = new QuestionRepository(applicationContext);
+                return questionRepository;
+            }
+        }
+        public IRepository<Answer> Answers
+        {
+            get
+            {
+                if (answerRepository == null)
+                    answerRepository = new AnswerRepository(applicationContext);
+                return answerRepository;
+            }
+        }
+        public IRepository<Element> Elements
+        {
+            get
+            {
+                if (elementRepository == null)
+                    elementRepository = new ElementRepository(applicationContext);
+                return elementRepository;
+            }
+        }
+        public IRepository<Entities.Models.Attribute> Attributes
+        {
+            get
+            {
+                if (attributeRepository == null)
+                    attributeRepository = new AttributeRepository(applicationContext);
+                return attributeRepository;
+            }
+        }
+        public IRepository<FormResult> FormResults
+        {
+            get
+            {
+                if (formResultRepository == null)
+                    formResultRepository = new FormResultRepository(applicationContext);
+                return formResultRepository;
+            }
+        }
+        public IRepository<QuestionResult> QuestionResults
+        {
+            get
+            {
+                if (questionResultRepository == null)
+                    questionResultRepository = new QuestionResultRepository(applicationContext);
+                return questionResultRepository;
+            }
+        }
+        public IRepository<AnswerResult> AnswerResults
+        {
+            get
+            {
+                if (answerResultRepository == null)
+                    answerResultRepository = new AnswerResultRepository(applicationContext);
+                return answerResultRepository;
+            }
+        }
+        public IRepository<AttributeResult> AttributeResults
+        {
+            get
+            {
+                if (attributeResultRepository == null)
+                    attributeResultRepository = new AttributeResultRepository(applicationContext);
+                return attributeResultRepository;
+            }
+        }
+        public void Dispose()
+        {
+            applicationContext.Dispose();
+        }
+        public void Save()
+        {
+            applicationContext.SaveChanges();
+        }
+    }
+}
---
+++ CFE.Infrastructure/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using CFE.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFE.Infrastructure.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<User> Users { get; }
+        IRepository<Form> Forms { get; }
+        IRepository<Question> Questions { get; }
+        IRepository<Answer> Answers { get; }
+        IRepository<Element> Elements { get; }
+        IRepository<CFE.Entities.Models.Attribute> Attributes { get; }
+        IRepository<FormResult> FormResults { get; }
+        IRepository<QuestionResult> QuestionResults { get; }
+        IRepository<AnswerResult> AnswerResults { get; }
+        IRepository<AttributeResult> AttributeResults { get; }
+        void Save();
+    }
+}
