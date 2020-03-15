40a23968838d7327c3c0b362f05b581421d569c4
Kyrylo Voronkov <kavoronkov@gmail.com>
2020-03-15 19:55:34 +0200

    15032020_BL_DTO

---
+++ CFE.BLL/BL/AttributeBL.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using CFE.BLL.DTO;
+using CFE.Entities.Models;
+using CFE.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFE.BLL.BL
+{
+    public class AttributeBL : IRepository<AttributeDTO>
+    {
+        private IUnitOfWork unitOfWork;
+        private IMapper mapper;
+        public AttributeBL(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+            mapper = new MapperConfiguration(config => config.CreateMap<CFE.Entities.Models.Attribute, AttributeDTO>()).CreateMapper();
+        }
+        public void Create(AttributeDTO attributeDTO)
+        {
+            unitOfWork.Attributes.Create(mapper.Map<CFE.Entities.Models.Attribute>(attributeDTO));
+            unitOfWork.Save();
+        }
+        public void Delete(int id)
+        {
+            unitOfWork.Attributes.Delete(id);
+            unitOfWork.Save();
+        }
+        public AttributeDTO Read(int id) => mapper.Map<AttributeDTO>(unitOfWork.Attributes.Read(id));
+        public IEnumerable<AttributeDTO> ReadAll() => mapper.Map<IEnumerable<CFE.Entities.Models.Attribute>, List<AttributeDTO>>(unitOfWork.Attributes.ReadAll());
+        public void Update(AttributeDTO attributeDTO)
+        {
+            unitOfWork.Attributes.Update(mapper.Map<CFE.Entities.Models.Attribute>(attributeDTO));
+            unitOfWork.Save();
+        }
+    }
+}
---
+++ CFE.BLL/BL/FormBL.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using CFE.BLL.DTO;
+using CFE.Entities.Models;
+using CFE.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFE.BLL.BL
+{
+    public class FormBL : IRepository<FormDTO>
+    {
+        private IUnitOfWork unitOfWork;
+        private IMapper mapper;
+        public FormBL(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+            mapper = new MapperConfiguration(config => config.CreateMap<Form, FormDTO>()).CreateMapper();
+        }
+        public void Create(FormDTO formDTO)
+        {
+            unitOfWork.Forms.Create(mapper.Map<Form>(formDTO));
+            unitOfWork.Save();
+        }
+        public void Delete(int id)
+        {
+            unitOfWork.Forms.Delete(id);
+            unitOfWork.Save();
+        }
+        public FormDTO Read(int id) => mapper.Map<FormDTO>(unitOfWork.Forms.Read(id));
+        public IEnumerable<FormDTO> ReadAll() => mapper.Map<IEnumerable<Form>, List<FormDTO>>(unitOfWork.Forms.ReadAll());
+        public void Update(FormDTO formDTO)
+        {
+            unitOfWork.Forms.Update(mapper.Map<Form>(formDTO));
+            unitOfWork.Save();
+        }
+    }
+}
