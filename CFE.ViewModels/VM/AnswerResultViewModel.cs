24f39504d4f6fb6c11ad33af20eede0fe0f4591d
Kyrylo Voronkov <kavoronkov@gmail.com>
2020-03-15 23:49:01 +0200

    15032020_ViewNodel

---
+++ CFE.ViewModels/VM/AnswerResultViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFE.ViewModels.VM
+{
+    public class AnswerResultViewModel
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public int QuestionResultId { get; set; }
+    }
+}
---
+++ CFE.ViewModels/VM/AttributeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CFE.ViewModels.VM
+{
+    public class AttributeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public int ElementId { get; set; }
+    }
+}
