a889e034d0a8b593b14839acd85abd6a190c0e9f
Sofiia Halkina <gloria34@dlit.dp.ua>
2020-04-21 23:10:00 +0300

    fix: renamed ElementId -> QuestionId in Attribute.sql

--- CFE.DAL.SSDT/dbo/Tables/Attribute.sql
+++ CFE.DAL.SSDT/dbo/Tables/Attribute.sql
@@ -2,13 +2,13 @@
     [Id]          INT            IDENTITY (100, 1) NOT NULL,
     [Name]        NVARCHAR (MAX) NOT NULL,
     [DisplayName] NVARCHAR (MAX) NOT NULL,
-    [ElementId]   INT            NOT NULL,
+    [QuestionId]   INT            NOT NULL,
     CONSTRAINT [PK_Attribute] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Attribute_Element_ElementId] FOREIGN KEY ([ElementId]) REFERENCES [dbo].[Element] ([Id])
+    CONSTRAINT [FK_Attribute_Element_ElementId] FOREIGN KEY ([QuestionId]) REFERENCES [dbo].[Question] ([Id])
 );
 
 
 GO
 CREATE NONCLUSTERED INDEX [IX_Attribute_ElementId]
-    ON [dbo].[Attribute]([ElementId] ASC);
+    ON [dbo].[Attribute]([QuestionId] ASC);
 
