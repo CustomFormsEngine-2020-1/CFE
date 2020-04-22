c3b10c32abe756ef8c4b8e6e4ab5dc852c13e2b6
konstantinshikhov <51083454+konstantinshikhov@users.noreply.github.com>
2020-04-22 10:02:01 +0300

    work with change password

--- CFE.WebUI/Controllers/UserController.cs
+++ CFE.WebUI/Controllers/UserController.cs
@@ -1,12 +1,77 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using CFE.ViewModels.VM.Users;
+using CFE.Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
 
 namespace CFE.WebUI.Controllers
 {
     public class UserController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public UserController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> ChangePassword()
+        {
+            string id = "c6dcad79-196a-4cc8-b017-5738bdc43e5b";
+            //var id2 = User.FindFirst(Claims.UserId).Value;
+            
+            User user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ChangePasswordViewModel model = new ChangePasswordViewModel { Id = user.Id, Email = user.Email };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    var _passwordValidator =
+                        HttpContext.RequestServices.GetService(typeof(IPasswordValidator<User>)) as IPasswordValidator<User>;
+                    var _passwordHasher =
+                        HttpContext.RequestServices.GetService(typeof(IPasswordHasher<User>)) as IPasswordHasher<User>;
+
+                    IdentityResult result =
+                        await _passwordValidator.ValidateAsync(_userManager, user, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        user.PasswordHash = _passwordHasher.HashPassword(user, model.NewPassword);
+                        await _userManager.UpdateAsync(user);
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Пользователь не найден");
+                }
+            }
+            return View(model);
         }
 
     }
